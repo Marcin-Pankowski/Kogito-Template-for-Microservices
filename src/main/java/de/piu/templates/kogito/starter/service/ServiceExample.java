ac92832a836be42f93cbb14622a3fcb0007f6540
Marcin Pankowski <mp@piu.de>
2023-11-22 12:56:29 +0100

    redme revised

--- src/main/java/de/piu/templates/kogito/starter/service/ServiceExample.java
+++ src/main/java/de/piu/templates/kogito/starter/service/ServiceExample.java
@@ -1,5 +1,12 @@
 package de.piu.templates.kogito.starter.service;
 
+import javax.inject.Singleton;
+
+@Singleton
 public class ServiceExample {
+
+    public void doSomething() {
+        System.out.println("TEST");
+    }   
     
 }
