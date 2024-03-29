8ee1ba34243d0fe8d10b587b9a564ef77fdd645e
Marcin Pankowski <mp@piu.de>
2024-03-30 00:22:48 +0100

    added Custom Controller Example

--- src/main/java/de/piu/templates/kogito/controller/CustomControllerExample.java
+++ src/main/java/de/piu/templates/kogito/controller/CustomControllerExample.java
@@ -6,16 +6,24 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
+import org.kie.kogito.incubation.application.AppRoot;
+import de.piu.templates.kogito.dto.Person;
+
+import java.io.StringReader;
+
 
 @Path("/custom-rest-Endpoint")
 public class CustomControllerExample {
 
+    @Inject
+    AppRoot appRoot;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public String returnString() {
+    @Produces(MediaType.TEXT_PLAIN)
+    public String returnString(Person person) {
 
-        return "Hello World!";
+
+        return person.getName() + " " + person.getAge();
     }
 }
