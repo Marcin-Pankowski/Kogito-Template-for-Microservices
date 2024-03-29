13f16fa49a41777c30a8053e79a8f9604cfa7ef0
Marcin Pankowski <mp@piu.de>
2024-03-30 00:23:42 +0100

    cleanup imports

--- src/main/java/de/piu/templates/kogito/controller/CustomControllerExample.java
+++ src/main/java/de/piu/templates/kogito/controller/CustomControllerExample.java
@@ -1,12 +1,10 @@
 package de.piu.templates.kogito.controller;
 
-import javax.inject.Inject;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
-import org.kie.kogito.incubation.application.AppRoot;
 import de.piu.templates.kogito.dto.Person;
 
 import java.io.StringReader;
@@ -14,9 +12,6 @@ import java.io.StringReader;
 
 @Path("/custom-rest-Endpoint")
 public class CustomControllerExample {
-
-    @Inject
-    AppRoot appRoot;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
